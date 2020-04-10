cfd9b83bff1378f7a68ebc7c1b068108e7dec83f
Scott Jespersen <sjespers@stanford.edu>
2020-04-10 13:11:08 -0700

    release

---
+++ precache-manifest.2e9e79d5ac793186ad59ddd2f7c606b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23825cba5ab6a2697354d19654ce0d35",
+    "url": "/energy-industry-maps/index.html"
+  },
+  {
+    "revision": "8c4c5a86c0d273769621",
+    "url": "/energy-industry-maps/static/css/main.49a1a99d.chunk.css"
+  },
+  {
+    "revision": "b63c847f882f4d9508e5",
+    "url": "/energy-industry-maps/static/js/2.57eed576.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/energy-industry-maps/static/js/2.57eed576.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8c4c5a86c0d273769621",
+    "url": "/energy-industry-maps/static/js/main.5e0e95e9.chunk.js"
+  },
+  {
+    "revision": "c64b56b5c933615e668e",
+    "url": "/energy-industry-maps/static/js/runtime-main.8c161b71.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.31380e6e154bd80fedb632cbcbabc2dc.js"
+  "/energy-industry-maps/precache-manifest.2e9e79d5ac793186ad59ddd2f7c606b7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/energy-industry-maps/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
