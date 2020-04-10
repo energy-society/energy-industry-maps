db1b120a0768890fcc57ff32d1674d86d3a921d1
Scott Jespersen <sjespers@stanford.edu>
2020-04-10 13:13:35 -0700

    finish up deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,18 @@ git fetch origin gh-pages
 git rm $(git ls-tree -r --name-only HEAD)
 
 # Make a backup copy of the build
+rm -r .build-cache
 cp -r build .build-cache
 
 # Move new build here
+git add build/*
 git mv build/* .
+
+# Commit and push
+git commit -m "release"
+git push origin gh-pages
+
+# Clean up
+git checkout master
+rm -r build
+mv .build-cache build
