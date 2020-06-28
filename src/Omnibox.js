e82b285cd21a9ea4a539cf56e6e6d3d67b4749cc
Scott Jespersen <sjespers@stanford.edu>
2020-06-28 10:53:45 -0700

    hook-ify omnibox

--- src/Omnibox.js
+++ src/Omnibox.js
@@ -1,104 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class Omnibox extends React.Component {
-  state = {
-    query: '',
-    hasFocus: false,
-    searchResults: [],
-  };
+export default function Omnibox(props) {
+  const [query, setQuery] = useState('');
+  const [hasFocus, setHasFocus] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
 
-  constructor(props) {
-    super(props);
-    this.getCompanies = this.getCompanies.bind(this);
-    this.shouldDisplaySuggestions = this.shouldDisplaySuggestions.bind(this);
-    this.setHasFocus = this.setHasFocus.bind(this);
-    this.handleResultSelection = this.handleResultSelection.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
-  }
-
-  getCompanies() {
-    if (this.props.companies) {
-      return this.props.companies.map(f => f.properties.company);
+  function getCompanies() {
+    if (props.companies) {
+      return props.companies.map(f => f.properties.company);
     }
     return [];
   }
 
-  shouldDisplaySuggestions() {
-    return this.state.hasFocus && this.state.searchResults.length > 0;
+  const shouldDisplaySuggestions = () => hasFocus && searchResults.length > 0;
+
+  function handleResultSelection(value) {
+    props.onSelectCompany(value);
+    setQuery(value);
+    setSearchResults([]);
   }
 
-  setHasFocus(hasFocus) {
-    this.setState({hasFocus: hasFocus});
-  }
-
-  handleResultSelection(value) {
-    this.props.onSelectCompany(value);
-    this.setState({query: value, searchResults: []});
-  }
-
-  handleInputChange() {
-    const query = this.searchInput.value;
-    this.setState({query: query});
+  function handleInputChange(e) {
+    let query = e.target.value;
+    setQuery(query);
     if (query.length >= 2) {
-      this.setState({
-        searchResults: this.getCompanies().filter(
-          result => result.toLowerCase().includes(query.toLowerCase()))
-      });
+      setSearchResults(getCompanies().filter(
+          result => result.toLowerCase().includes(query.toLowerCase())));
     } else {
-      this.setState({searchResults: []});
+      setSearchResults([]);
     }
   }
 
-  render() {
-    const searchSuggestions = this.state.searchResults.map(r => (
-      <li key={r} tabIndex="-1" onMouseDown={() => this.handleResultSelection(r)}>{r}</li>));
-    return (
-      <div className="omnibox">
-        <button
-          className="omnibox-burger-menu"
-          onClick={this.props.onOpenSettingsPane}
-          title="Menu"
-          aria-label="Menu">
-          <span>
-            <span
-              className="omnibox-burger-bar omnibox-burger-bar-top"
-              aria-hidden="true" />
-            <span
-              className="omnibox-burger-bar omnibox-burger-bar-middle"
-              aria-hidden="true" />
-            <span
-              className="omnibox-burger-bar omnibox-burger-bar-bottom"
-              aria-hidden="true" />
-          </span>
-        </button>
-        <div className="omnibox-search-input-container">
-          <input
-            type="text"
-            className="omnibox-search-input"
-            id="omnibox-search-input"
-            ref={input => this.searchInput = input}
-            onChange={this.handleInputChange}
-            tabIndex="0"
-            value={this.state.query}
-            onFocus={() => this.setState({hasFocus: true})}
-            onBlur={() => this.setHasFocus(false)}
-            placeholder="Search..." />
-        </div>
-        <div
-          className="omnibox-search-suggestions-container"
-          tabIndex="-1"
-          style={{display: this.shouldDisplaySuggestions() ? "block" : "none"}}>
-          <ul className="omnibox-search-suggestions" tabIndex="-1">{searchSuggestions}</ul>
-        </div>
-        <button
-          className="omnibox-search-button"
-          title="Search"
-          aria-label="Search"
-          onClick={() => document.getElementById("omnibox-search-input").focus()}>
-          <div className="magnifying-glass" aria-hidden="true">&#9906;</div>
-        </button>
-      </div>);
-  }
-}
+  const searchSuggestions = searchResults.map(r => (
+    <li
+      key={r}
+      tabIndex="-1"
+      onMouseDown={() => handleResultSelection(r)}>
+      {r}
+    </li>));
 
-export default Omnibox;
+  return (
+    <div className="omnibox">
+      <button
+        className="omnibox-burger-menu"
+        onClick={props.onOpenSettingsPane}
+        title="Menu"
+        aria-label="Menu">
+        <span>
+          <span
+            className="omnibox-burger-bar omnibox-burger-bar-top"
+            aria-hidden="true" />
+          <span
+            className="omnibox-burger-bar omnibox-burger-bar-middle"
+            aria-hidden="true" />
+          <span
+            className="omnibox-burger-bar omnibox-burger-bar-bottom"
+            aria-hidden="true" />
+        </span>
+      </button>
+      <div className="omnibox-search-input-container">
+        <input
+          type="text"
+          className="omnibox-search-input"
+          id="omnibox-search-input"
+          onChange={handleInputChange}
+          tabIndex="0"
+          value={query}
+          onFocus={() => setHasFocus(true)}
+          onBlur={() => setHasFocus(false)}
+          placeholder="Search..." />
+      </div>
+      <div
+        className="omnibox-search-suggestions-container"
+        tabIndex="-1"
+        style={{display: shouldDisplaySuggestions() ? "block" : "none"}}>
+        <ul className="omnibox-search-suggestions" tabIndex="-1">{searchSuggestions}</ul>
+      </div>
+      <button
+        className="omnibox-search-button"
+        title="Search"
+        aria-label="Search"
+        onClick={() => document.getElementById("omnibox-search-input").focus()}>
+        <div className="magnifying-glass" aria-hidden="true">&#9906;</div>
+      </button>
+    </div>);
+}
