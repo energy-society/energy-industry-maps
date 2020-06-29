5a5a13ffbb6e323e2c520f96085f80a951874737
energy-society <energy-society@users.noreply.github.com>
2020-06-29 01:14:25 +0000

    deploy: a1aa7509ff43f20a27caa333b1aeda5421912e81

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/energy-industry-maps/precache-manifest.7cf059aabc3976016b0885bd0cc9ca26.js"
+  "/energy-industry-maps/precache-manifest.4a91f2f1b1c6e24ded28123b0cbe43bb.js"
 );
 
 self.addEventListener('message', (event) => {
