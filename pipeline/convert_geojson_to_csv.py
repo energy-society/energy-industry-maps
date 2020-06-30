ff93a2e3ec7a396c280ca683a9a87c756c19cc9d
Scott Jespersen <sjespers@stanford.edu>
2020-06-30 12:45:29 -0700

    clean up conversion script

--- pipeline/convert_geojson_to_csv.py
+++ pipeline/convert_geojson_to_csv.py
@@ -4,28 +4,36 @@ import sys
 
 USAGE = f"python {__file__} <input_geojson> <output_csv>"
 
-if len(sys.argv) != 3:
-    print(USAGE)
-    sys.exit(1)
 
-geojson_input = sys.argv[1]
-csv_output = sys.argv[2]
+def convert(geojson):
+    records = []
+    for feature in geojson['features']:
+        record = {}
+        for prop, val in feature['properties'].items():
+            if prop:
+                record[prop] = val
+        coords = feature['geometry']['coordinates']
+        record['lat'] = coords[1]
+        record['lng'] = coords[0]
+        records.append(record)
 
-with open(geojson_input) as f:
-    geojson = json.load(f)
+    df = pd.DataFrame.from_records(records)
+    df.index = df.idx
+    df = df.drop(['idx'], axis=1)
+    return df.to_csv()
 
-records = []
-for feature in geojson['features']:
-    record = {}
-    for prop, val in feature['properties'].items():
-        if prop:
-            record[prop] = val
-    coords = feature['geometry']['coordinates']
-    record['lat'] = coords[1]
-    record['lng'] = coords[0]
-    records.append(record)
 
-df = pd.DataFrame.from_records(records)
-df.index = df.idx
-df = df.drop(['idx'], axis=1)
-df.to_csv(csv_output)
+def main():
+    if len(sys.argv) != 3:
+        print(USAGE)
+        sys.exit(1)
+
+    geojson_input, csv_output = sys.argv[1:3]
+    with open(geojson_input) as f:
+        geojson = json.load(f)
+    with open(csv_output, 'w') as f:
+        f.write(convert(geojson))
+
+
+if __name__ == '__main__':
+    main()
